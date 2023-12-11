8a91cbf3469f693df8af530d12b39c002ecce398
Sava <50882455+sava41@users.noreply.github.com>
2023-12-10 20:06:06 -0500

    build system added

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <windows.h>
+#include <MinHook.h>
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <dxgi.h>
+#include <psapi.h>
+#include <cstdio>
+#include <intrin.h>
+#include <vector>
+
+#include <io.h>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    std::cout << "Hello World!";
+    return 0;
+}
